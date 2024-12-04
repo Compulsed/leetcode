b4f9454338c22347c6541b86c0e1e5ec854e5a6a
Dale Salter <dale@mini.dalejsalter.com>
2024-12-04 09:39:58 -0500

    ADDED: Largest island

--- largest-island-ts/index.ts
+++ largest-island-ts/index.ts
@@ -1,14 +1,8 @@
+import assert from 'node:assert'
+import { describe, it } from 'node:test'
 
-
-const inputGrid = [
-    [0, 1, 0, 0, 0],
-    [1, 1, 0, 0, 0],
-    [1, 0, 0, 1],
-    [1, 0, 0, 1, 0],
-    [1, 0, 0, 1, 0]
-]
-
-
+// Optimization: isLand / isSeen could be the same value. Once we visit the
+//  land we can say it's no longer land.
 class Land {
     isLand: boolean
     isSeen: boolean
@@ -18,11 +12,6 @@ class Land {
         this.isSeen = false
     }
 }
-
-const landGrid: Land[][] = inputGrid.map(
-    r => r.map(l => new Land(l == 1))
-)
-
 
 class Point {
     x: number
@@ -34,7 +23,7 @@ class Point {
     }
 }
 
-const calculateSize = (point: Point): number => {
+const calculateSize = (landGrid: Land[][], point: Point): number => {
     // Bounds check Y axis
     if (point.y < 0 || point.y >= landGrid.length)
         return 0
@@ -58,27 +47,31 @@ const calculateSize = (point: Point): number => {
     let count = 1
 
     // Up
-    count += calculateSize(new Point(point.x, point.y - 1))
+    count += calculateSize(landGrid, new Point(point.x, point.y - 1))
 
     // Down
-    count += calculateSize(new Point(point.x, point.y + 1))
+    count += calculateSize(landGrid, new Point(point.x, point.y + 1))
 
     // Left
-    count += calculateSize(new Point(point.x - 1, point.y))
+    count += calculateSize(landGrid, new Point(point.x - 1, point.y))
 
     // Right
-    count += calculateSize(new Point(point.x + 1, point.y))
+    count += calculateSize(landGrid, new Point(point.x + 1, point.y))
     
     return count;
 }
 
-const main = () => {
+const calculateLargest = (inputGrid: number[][]): number => {
+    const landGrid: Land[][] = inputGrid.map(
+        r => r.map(l => new Land(l == 1))
+    )    
+
     let largest = 0;
 
     landGrid.forEach((r, y) => r.forEach((_, x) => {
         let point = new Point(x, y);
 
-        let size = calculateSize(point)
+        let size = calculateSize(landGrid, point)
 
         if (size > 0) {
             console.log(`Found new island of size: ${size} at point ${JSON.stringify(point)}`)
@@ -90,6 +83,63 @@ const main = () => {
     }))
 
     console.log("Largest size: ", largest)
+    
+    return largest
 }
 
-main()
+
+
+describe('calculateLargest', () => {
+    it('happy case should work', () => {
+        const inputGrid = [
+            [0, 1, 0, 0, 0],
+            [1, 1, 0, 0, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 1, 0],
+            [1, 0, 0, 1, 0]
+        ]
+    
+        assert.strictEqual(calculateLargest(inputGrid), 6)
+    })
+
+    it('grid off different sizes should not raise an error', () => {
+        const inputGrid = [
+            [0, 1, 0, 0, 0],
+            [1, 1],
+        ]
+    
+        // Assertion: Should not throw any kind of exception or error
+        assert.strictEqual(calculateLargest(inputGrid), 3)
+    })
+
+    it('if there are no islands should return a size of 0', () => {
+        const inputGrid = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+        ]
+    
+        // Assertion: Should not throw any kind of exception or error
+        assert.strictEqual(calculateLargest(inputGrid), 0)
+    })
+
+
+    it('if there is no land we should return 0', () => {
+        const inputGrid = []
+    
+        // Assertion: Should not throw any kind of exception or error
+        assert.strictEqual(calculateLargest(inputGrid), 0)
+    })    
+
+    it('if there is multiple islands the largest island should be returned', () => {
+        const inputGrid = [
+            [1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1],
+            [0, 0, 0, 1, 1]
+        ]
+    
+        assert.strictEqual(calculateLargest(inputGrid), 3)
+    })       
+})
+

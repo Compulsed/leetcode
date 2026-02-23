ab57745ee9ac9eddf64621aa3ab9b0db30efe464
Dale <dale@air.local>
2026-02-23 14:45:51 -0500

    ADDED: Changes

--- graph/currency/index.ts
+++ graph/currency/index.ts
@@ -1,29 +1,33 @@
+/*
+    Improvements:
+    - Use Record for defining dicts
+    - ??= for assigning an empty dict if not defined
+    - Use Set rather than another dict (.has(), add())
+    - Use Object.entries in a for of loop for early returns
+    - Object.entries can be used in array AND dict (index value, dict value)
+    - ! is force skip null check
 
+    Mental model:
+    1. Draw our example data structure as a comment in psudocode
+    2. Consider input edge cases
+*/
 
 // Input: 
 // ['AUD', 'USD', 1.33]
 // ['USD', 'GBP', 2]
 
-const currencyConversion = [
+const currencyConversion: [string, string, number][] = [
     ['AUD', 'USD', 0.71],
     ['AUD', 'IDR', 11873],
     ['USD', 'GBP', 0.74],
     ['EUR', 'GBP', 0.87],
 ]
 
-/*
-    { AUD: { USD: 1.33, IDR: 50 }}
-*/
-const currencyConversionDict: any = {}
+const currencyConversionDict: Record<string, Record<string, number>> = {}
 
 currencyConversion.forEach(([from, to, coefficient ]) => {
-    if (!currencyConversionDict[from]) {
-        currencyConversionDict[from] = {}
-    }
-
-    if (!currencyConversionDict[to]) {
-        currencyConversionDict[to] = {}
-    }    
+    currencyConversionDict[from] ??= {}
+    currencyConversionDict[to] ??= {}
 
     currencyConversionDict[from][to] = coefficient
     currencyConversionDict[to][from] = (1 / coefficient)
@@ -45,19 +49,19 @@ currencyConversion.forEach(([from, to, coefficient ]) => {
 
 const NOT_FOUND = -1
 
-const dfs = (currentCurreny: string, toFindCurrency: string, seenNodes: Set<String>): number => {
+const dfs = (currentCurrency: string, toFindCurrency: string, seenNodes: Set<string>): number => {
+    if (currentCurrency === toFindCurrency) {
+        return 1
+    }
+
     // Already visited
-    if (seenNodes.has(currentCurreny)) {
+    if (seenNodes.has(currentCurrency)) {
         return NOT_FOUND
     }
 
-    seenNodes.add(currentCurreny)
+    seenNodes.add(currentCurrency)
 
-    // [ 'USD', '1.33' ]
-    const searchCurrencies = Object.entries(currencyConversionDict[currentCurreny] || {});
-
-    for (const currencyPair of searchCurrencies) {
-        const [symbol, coefficient] = currencyPair as any
+    for (const [symbol, coefficient] of Object.entries(currencyConversionDict[currentCurrency] || {})) {
 
         if (toFindCurrency === symbol) {
             return coefficient
@@ -65,7 +69,7 @@ const dfs = (currentCurreny: string, toFindCurrency: string, seenNodes: Set<Stri
 
         const value = dfs(symbol, toFindCurrency, seenNodes)
 
-        if (value === -1) {
+        if (value === NOT_FOUND) {
             continue
         }
 
@@ -75,8 +79,44 @@ const dfs = (currentCurreny: string, toFindCurrency: string, seenNodes: Set<Stri
     return -1 
 }
 
+
+const bfs =  (fromCurrency: string, toFindCurrency: string): number => {
+    const queue = [{
+        currencyToCheck: fromCurrency,
+        currentCoefficient: 1
+    }]
+
+    const seenCurrencies = new Set<string>([fromCurrency])
+
+    while (queue.length) {
+        // Save check because we do a queue length check
+        const { currencyToCheck, currentCoefficient } = queue.shift()!;
+
+        if (currencyToCheck === toFindCurrency) {
+            return currentCoefficient 
+        }
+
+        for (const [currency, coefficient] of Object.entries(currencyConversionDict[currencyToCheck] || {})) {
+            if (seenCurrencies.has(currency)) {
+                continue
+            }
+
+            // Mark seen so we do not check again
+            seenCurrencies.add(currencyToCheck)            
+
+            queue.push({
+                currencyToCheck: currency,
+                currentCoefficient: coefficient * currentCoefficient
+            })
+        }
+    }
+    
+    return NOT_FOUND
+}
+
 const convert = (fromCurrency: string, toCurrency: string, amount: number) => {
-    const searchResult = dfs(fromCurrency, toCurrency, new Set())
+    // const searchResult = dfs(fromCurrency, toCurrency, new Set())
+    const searchResult = bfs(fromCurrency, toCurrency)
 
     if (searchResult !== -1) {
         return amount * searchResult
@@ -85,4 +125,4 @@ const convert = (fromCurrency: string, toCurrency: string, amount: number) => {
     return "Not Found"
 }
 
-console.log(convert('AUD', 'EUR', 10))
+console.log(convert('AUD', 'AAA', 10))
